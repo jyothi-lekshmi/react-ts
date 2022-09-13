b7088bf5e0dae40df17aff51eea02e45f0613dc3
jyothi-lekshmi <109862279+jyothi-lekshmi@users.noreply.github.com>
2022-09-13 19:22:40 +0530

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import Forms from './component/form';
+import List from './component/list';
 import './style.css';
 const App: React.FC = () => {
-  return <Forms />;
+  return (
+    <div>
+      {/* <Forms /> */}
+      <List />
+    </div>
+  );
 };
 export default App;
---
+++ component/list.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import '../style.css';
+
+const List: React.FC = () => {
+  return (
+    <div className="row">
+      <div className="column">
+        <div className="card">
+        <table>
+          <thead>
+            <tr>
+              <td colSpan={2}> <b>List of People</b></td>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>First Name</td>
+            <td>Jyothi</td>
+          </tr>
+          <tr>
+            <td>Last Name</td>
+            <td>Jyothi</td>
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>Jyothi</td>
+          </tr>
+          <tr>
+            <td>Telephone</td>
+            <td>Jyothi</td>
+          </tr>
+          </tbody>
+        </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default List;
