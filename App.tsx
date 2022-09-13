073c6e31f557fc6a4b6f3b547a7b6295ad765160
jyothi-lekshmi <109862279+jyothi-lekshmi@users.noreply.github.com>
2022-09-13 17:53:23 +0530

    4 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react';
 import './style.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello !</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="text" placeholder="Last Name" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="text" placeholder="Email" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Telephone</Form.Label>
+        <Form.Control type="text" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary text-left" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
-}
+};
+export default App;
